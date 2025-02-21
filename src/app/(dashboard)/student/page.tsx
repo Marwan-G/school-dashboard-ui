e315e5bd5b40aed4cb9215df503fbac2004eea54
Marwan-G <ghabin2004@gmail.com>
2025-02-21 23:17:23 +0100

    student page

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
+import EventCalendar from "@/components/EventCalendar";
+import AnnouncmentChart from "@/components/AnnouncmentChart";
 
 const page = () => {
   return (
-    <div>
-      <h4>this is student page</h4>
+    <div className="p-4  h-screen flex gap-4  md:flex-row">
+      {/* LEFT    */}
+      <div className="w-full lg:w-2/3 ">
+      <div className="bg-white p-4 rounded-md">
+        <h1>schedule (A4)</h1>
+      </div>
+      </div>
+      {/* RIGH  ChART  */}
+      <div className="w-full flex-col lg:w-1/3 flex  gap-8 ">
+        <EventCalendar />
+        <AnnouncmentChart />
+      </div>
     </div>
-  )
-}
-export default page
+  );
+};
+export default page;
