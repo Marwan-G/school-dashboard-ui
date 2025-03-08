a080c64d4baeb1797abcd6e8c8d9d19fe22e611a
Marwan-G <ghabin2004@gmail.com>
2025-03-08 20:45:35 +0100

    table header

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const Table = ( {columns}:{columns:{header:string; accessor:string; ClassName?:string}}) => {
+const Table = ({
+  columns,
+}: {
+  columns: { header: string; accessor: string; ClassName?: string }[];
+}) => {
   return (
-    <table className='w-fll mt-4'>
-      <thead>
-
+    <table className="w-fll mt-4">
+      <thead className="">
+        <tr className="text-left text-gray-500 text-sm">
+          {columns.map((col) => (
+            <th key={col.accessor}> {col.header}</th>
+          ))}
+        </tr>
       </thead>
     </table>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
