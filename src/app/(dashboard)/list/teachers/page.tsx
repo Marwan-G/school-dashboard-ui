177e470bb2293fcea68ed499b2cb1c7471dc5cf1
Marwan-G <ghabin2004@gmail.com>
2025-03-13 21:30:45 +0100

    update teacher table

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -4,8 +4,8 @@ import Table from "@/components/Table";
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
-
-type Teacher = {
+import { role, teachersData } from "@/lib/data";
+export type Teacher = {
   id: number;
   teacherId: string;
   name: string;
@@ -55,36 +55,43 @@ const TeacherListPage = () => {
   ];
 
   const rendRow = (item: Teacher) => {
-    <tr>
-      <td>
-        <Image
-          src={item.photo}
-          alt=""
-          width={40}
-          height={40}
-          className="md:hidden xl:block w-10 h-10 rounded-full object-cover "
-        />
-        <div className="flex flex-co">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item?.email}</p>
-        </div>
-      </td>
-       <td>{item.teacherId}</td>
-     
-      <td className="hidden md:table-cell" >{item.subjects.join(",")}</td>
-      <td className="hidden md:table-cell">{item.classes.join(",")}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
-          <button>
-            
-          </button>
-          </Link>
-        </div>
-      </td>
-    </tr>;
+    return (
+      <tr key={item.id}>
+        <td>
+          <Image
+            src={item.photo}
+            alt=""
+            width={40}
+            height={40}
+            className="md:hidden xl:block w-10 h-10 rounded-full object-cover "
+          />
+          <div className="flex flex-co">
+            <h3 className="font-semibold">{item.name}</h3>
+            <p className="text-xs text-gray-500">{item?.email}</p>
+          </div>
+        </td>
+        <td>{item.teacherId}</td>
+
+        <td className="hidden md:table-cell">{item.subjects.join(",")}</td>
+        <td className="hidden md:table-cell">{item.classes.join(",")}</td>
+        <td className="hidden md:table-cell">{item.phone}</td>
+        <td className="hidden md:table-cell">{item.address}</td>
+        <td>
+          <div className="flex items-center gap-2">
+            <Link href={`/list/teachers/${item.id}`}>
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-blue-200">
+                <Image src="/view.png" alt="" width={16} height={16} />
+              </button>
+            </Link>
+            {role === "admin" && (
+              <button className="w-7 h-7 flex items-center justify-center rounded-full bg-purple-300">
+                <Image src="/delete.png" alt="" width={16} height={16} />
+              </button>
+            )}
+          </div>
+        </td>
+      </tr>
+    );
   };
   return (
     <div className=" bg-white p-4 rounded-md flex-1 m-4s mt-0">
@@ -108,7 +115,7 @@ const TeacherListPage = () => {
       </div>
       {/* LIST */}
       <div className="">
-        <Table columns={columns} />
+        <Table columns={columns} rendRow={rendRow} data={teachersData} />
       </div>
       {/* PAGINATION */}
       <div className="">
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,31 @@
 import React from "react";
+import { Teacher } from "@/app/(dashboard)/list/teachers/page";
 
 const Table = ({
   columns,
+  rendRow,
+  data, // added data prop
 }: {
   columns: { header: string; accessor: string; ClassName?: string }[];
+  rendRow: (item:Teacher) => React.ReactNode; // Define rendRow function properly
+  // data: Teacher[]; // Define data prop
+  data: any[]
 }) => {
   return (
-    <table className="w-fll mt-4 border-1">
+    <table className="w-fll mt-4">
       <thead className="">
-        <tr className="text-left text-gray-500 text-sm ">
+        <tr className="text-left text-gray-500 text-sm">
           {columns.map((col) => (
-            <th  key={col.accessor}> {col.header}</th>
+            <th key={col.accessor}> {col.header}</th>
           ))}
         </tr>
       </thead>
+      <tbody>
+        {data.map((item) =>
+          // Render rows for each teacher
+          rendRow(item)
+        )}
+      </tbody>
     </table>
   );
 };
